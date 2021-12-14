c51010544dcd0bb5f13a51df63ad7c338e7e8c60
Nathan Evans <github@talkswithnumbers.com>
2021-12-14 13:14:22 -0800

    Fix up fluent theme shading

--- packages/fluent/src/FluentTheme.ts
+++ packages/fluent/src/FluentTheme.ts
@@ -2,61 +2,35 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import {
-	BaseSlots,
-	IThemeRules,
-	themeRulesStandardCreator,
-	ThemeGenerator,
-	createTheme,
-	ITheme,
-} from '@fluentui/react'
+import { createTheme, ITheme } from '@fluentui/react'
 import { FluentTheme as IFluentTheme } from './interfaces'
+import { themeJson } from './themeJson'
 import { Theme, ThemeVariant } from '@thematic/core'
 
-const initialize = (colors, inverted: boolean): IThemeRules => {
-	const themeRules = themeRulesStandardCreator()
-	ThemeGenerator.insureSlots(themeRules, inverted)
-	ThemeGenerator.setSlot(
-		themeRules[BaseSlots[BaseSlots.primaryColor]],
-		colors.primaryColor,
-	)
-	ThemeGenerator.setSlot(
-		themeRules[BaseSlots[BaseSlots.foregroundColor]],
-		colors.textColor,
-	)
-	ThemeGenerator.setSlot(
-		themeRules[BaseSlots[BaseSlots.backgroundColor]],
-		colors.backgroundColor,
-	)
-	ThemeGenerator.insureSlots(themeRules, inverted)
-	return themeRules
-}
-
+/**
+ * Wraps a thematic theme to output Fluent palettes.
+ * TODO: it would be nice if this implemented Fluent's theme interface
+ * directly so it could be passed as is to a ThemeContext.
+ */
 export class FluentTheme implements IFluentTheme {
 	private _theme: Theme
-	private _themeRules: IThemeRules
 	constructor(theme: Theme) {
-		const colors = {
-			primaryColor: theme.application().accent().hex(),
-			textColor: theme.application().foreground().hex(),
-			backgroundColor: theme.application().background().hex(),
-		}
-		const themeRules = initialize(colors, theme.variant === ThemeVariant.Dark)
 		this._theme = theme
-		this._themeRules = themeRules
 	}
 	get theme(): Theme {
 		return this._theme
 	}
 	toFluent(): ITheme {
+		const inverted = this._theme.variant === ThemeVariant.Dark
+		const json = themeJson(this._theme)
 		return createTheme({
-			...{ palette: this.toJSON() },
-			isInverted: this._theme.variant === ThemeVariant.Dark,
+			...{ palette: json },
+			isInverted: inverted,
 		})
 	}
-	toJSON(): { [key: string]: string } {
+	toJSON(): Record<string, string> {
 		// the full theme includes a variety of additional color mappings, for this we just want the core palette
-		const json = ThemeGenerator.getThemeAsJson(this._themeRules)
+		const json = themeJson(this._theme)
 		const jsonkeys = [
 			...this.primaryKeys,
 			...this.foregroundKeys,
---
+++ packages/fluent/src/__tests__/shades.spec.ts
@@ -0,0 +1,72 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import { distance } from 'chroma-js'
+import { correctShades } from '../shades'
+
+describe('theme shading correction functions', () => {
+	// many of the shades for these sets will be a perfect match, but there are a few with slight
+	// differences in the resulting hex code. none should be greater than 3, which is very close.
+	// this was computed with a distance minimizing function for each shade (resulting config in shades.ts)
+	const colorDistance = 3
+
+	test('fluent default example', () => {
+		// this is the default fluent palette
+		// see https://github.com/microsoft/fluentui/blob/master/packages/react/src/components/ThemeGenerator/ThemeRulesStandard.ts
+		const palette = {
+			foregroundColor: '#323130',
+			backgroundColor: '#ffffff',
+			black: '#000000',
+			neutralDark: '#201f1e',
+			neutralLight: '#edebe9',
+			neutralLighter: '#f3f2f1',
+			neutralLighterAlt: '#faf9f8',
+			neutralPrimary: '#323130',
+			neutralPrimaryAlt: '#3b3a39',
+			neutralQuaternary: '#d0d0d0',
+			neutralQuaternaryAlt: '#e1dfdd',
+			neutralSecondary: '#605e5c',
+			neutralTertiary: '#a19f9d',
+			neutralTertiaryAlt: '#c8c6c4',
+			white: '#ffffff',
+		}
+
+		const fixed = correctShades(palette, false)
+
+		Object.keys(palette).forEach(key => {
+			const dist = distance(palette[key], fixed[key], 'rgb')
+			expect(dist).toBeLessThan(colorDistance)
+		})
+	})
+
+	test('fluent dark example', () => {
+		// there is no default dark fluent theme, so we used this exampe to tune the shades for similar results
+		// https://github.com/microsoft/fluentui/blob/789a3733b128569190319fce3fe2b46900b24896/packages/theme-samples/src/DarkCustomizations/DarkCustomizations.ts
+		const palette = {
+			foregroundColor: '#f3f2f1',
+			backgroundColor: '#1b1a19',
+			black: '#ffffff',
+			neutralDark: '#faf9f8',
+			neutralPrimary: '#f3f2f1',
+			neutralPrimaryAlt: '#c8c6c4',
+			neutralSecondary: '#a19f9d',
+			neutralSecondaryAlt: '#979693',
+			neutralTertiary: '#797775',
+			neutralTertiaryAlt: '#484644',
+			neutralQuaternary: '#3b3a39',
+			neutralQuaternaryAlt: '#323130',
+			neutralLight: '#292827',
+			neutralLighter: '#252423',
+			neutralLighterAlt: '#201f1e',
+			white: '#1b1a19',
+		}
+
+		const fixed = correctShades(palette, true)
+
+		Object.keys(palette).forEach(key => {
+			const dist = distance(palette[key], fixed[key], 'rgb')
+			expect(dist).toBeLessThan(colorDistance)
+		})
+	})
+})
