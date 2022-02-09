a4c4474da36edf5f07818ff44b0026fd255cc7cb
Nathan Evans <github@talkswithnumbers.com>
2022-02-08 18:10:09 -0800

    Improve index/app structure

--- packages/webapp/src/components/App/App.tsx
+++ packages/webapp/src/components/App/App.tsx
@@ -2,46 +2,49 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { Theme } from '@thematic/core'
 import { ThematicFluentProvider } from '@thematic/fluent'
 import { ApplicationStyles } from '@thematic/react'
+import { FC } from 'react'
 import { useTheme } from '../../state'
 import { ControlPanel } from '../ControlPanel'
 import { ThemeEditor } from '../ThemeEditor'
 import { ThemeViewer } from '../ThemeViewer'
 
 import './App.css'
+import { DataContext } from './DataContext'
 
-export interface AppProps {
-	theme: Theme
+const AppComponent = () => {
+	const theme = useTheme()
+	return (
+		<ThematicFluentProvider theme={theme}>
+			<ApplicationStyles />
+			<div className="App">
+				<div
+					className="controls"
+					style={{
+						backgroundColor: theme.application().faint().hex(),
+						borderBottom: `1px solid ${theme.application().border().hex()}`,
+					}}
+				>
+					<ControlPanel />
+				</div>
+				<div className="main">
+					<div className="editor">
+						<ThemeEditor />
+					</div>
+					<div className="viewer">
+						<ThemeViewer />
+					</div>
+				</div>
+			</div>
+		</ThematicFluentProvider>
+	)
 }
 
-const AppComponent = ({ theme }: AppProps) => (
-	<ThematicFluentProvider theme={theme}>
-		<ApplicationStyles />
-		<div className="App">
-			<div
-				className="controls"
-				style={{
-					backgroundColor: theme.application().faint().hex(),
-					borderBottom: `1px solid ${theme.application().border().hex()}`,
-				}}
-			>
-				<ControlPanel />
-			</div>
-			<div className="main">
-				<div className="editor">
-					<ThemeEditor />
-				</div>
-				<div className="viewer">
-					<ThemeViewer />
-				</div>
-			</div>
-		</div>
-	</ThematicFluentProvider>
-)
-
-export const App = () => {
-	const theme = useTheme()
-	return <AppComponent theme={theme} />
+export const App: FC = () => {
+	return (
+		<DataContext>
+			<AppComponent />
+		</DataContext>
+	)
 }
--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -4,17 +4,11 @@
  */
 import { render } from 'react-dom'
 import { App } from './components/App'
-import { DataContext } from './components/App/DataContext'
 
 function mount(): void {
 	try {
 		const root = document.getElementById('root')
-		render(
-			<DataContext>
-				<App />
-			</DataContext>,
-			root,
-		)
+		render(<App />, root)
 	} catch (err) {
 		console.error('error rendering application', err)
 	}
