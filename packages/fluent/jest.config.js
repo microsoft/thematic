636a9c8e03a4913cec3a6f16668a02a0e695fa70
Chris Trevino <chtrevin@microsoft.com>
2022-05-23 12:43:05 -0700

    add jest configs

---
+++ packages/fluent/jest.config.js
@@ -0,0 +1,7 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import { configure } from '@essex/jest-config'
+const configuration = configure()
+export default configuration
